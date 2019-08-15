c00a8300b030ec6b0444ea963cbbb8f6d7e995ed
Santiago Soler <santiago.r.soler@gmail.com>
2019-08-15 16:20:08 -0300

    Add point_per_block to __init__.py

--- eql_source_layouts/__init__.py
+++ eql_source_layouts/__init__.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-docstring
 # Import functions/classes to make the public API
 from ._version import get_versions
+from .layouts import point_per_block
 
 # Get the version number through versioneer
 __version__ = get_versions()["version"]
