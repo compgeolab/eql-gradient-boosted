ac8fff9fcaea5851e393ae3bf9d8213b1fe876ab
Santiago Soler <santiago.r.soler@gmail.com>
2019-09-24 16:40:47 -0300

    Add synthetic ground survey function

--- eql_source_layouts/__init__.py
+++ eql_source_layouts/__init__.py
@@ -2,7 +2,7 @@
 # Import functions/classes to make the public API
 from ._version import get_versions
 from .layouts import variable_relative_depth, block_reduce_points
-from .synthetic_model import airborne_survey, synthetic_model
+from .synthetic_model import airborne_survey, ground_survey, synthetic_model
 
 # Get the version number through versioneer
 __version__ = get_versions()["version"]
--- eql_source_layouts/synthetic_model.py
+++ eql_source_layouts/synthetic_model.py
@@ -141,3 +141,50 @@ def airborne_survey(region, center=(-42.25, -22.27)):
     # Drop longitude and latitude from dataframe
     data = data.filter(["easting", "northing", "altitude_m"])
     return data
+
+
+def ground_survey(region, cut_region=(25.9, 29.9, -28.5, -24.5)):
+    """
+    Create a synthetic ground survey based on South Africa gravity measurements
+
+    Parameters
+    ----------
+    region : tuple or list
+        Boundaries of the synthetic region where the observation points are going to be
+        located. Must be passed in the following order: (``east``, ``west``, ``south``,
+        ``north``, ...). All subsequent boundaries will be ignored. All boundaries
+        should be in Cartesian coordinates and in meters.
+    cut_region : tuple (optional)
+        Region to reduce the extension of the survey. Must be boundaries of the original
+        survey, in degrees.
+
+    Returns
+    -------
+    data : :class:`pandas.DataFrame`
+        Dataframe containing the coordinates of the flight line and its altitude on the
+        Cartesian coordinates for the synthetic model. All coordinates and altitude are
+        in meters.
+    """
+    # Fetch data
+    data = hm.datasets.fetch_south_africa_gravity()
+    # Keep only the latitudinal, longitudinal coordinates and altitude of measurement
+    data = data.filter(["longitude", "latitude", "elevation"])
+    # Cut the data into the cut_region
+    inside = vd.inside((data.longitude, data.latitude), cut_region)
+    data = data[inside]
+    # Project coordinates
+    projection = pyproj.Proj(proj="merc", lat_ts=(cut_region[2] + cut_region[3]) / 2)
+    data["easting"], data["northing"] = projection(
+        data.longitude.values, data.latitude.values
+    )
+    # Move projected coordinates to the boundaries of the region argument
+    w, e, s, n = region[:4]
+    data["easting"] = (e - w) / (data.easting.max() - data.easting.min()) * (
+        data.easting - data.easting.min()
+    ) + w
+    data["northing"] = (n - s) / (data.northing.max() - data.northing.min()) * (
+        data.northing - data.northing.min()
+    ) + s
+    # Drop longitude and latitude from dataframe
+    data = data.filter(["easting", "northing", "elevation"])
+    return data
