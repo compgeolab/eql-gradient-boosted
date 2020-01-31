2632a92f9f9bd11e9c539de623aa5b118ed41fae
Santiago Soler <santiago.r.soler@gmail.com>
2020-01-31 17:52:14 -0300

    Create depth types figure and save it

--- notebooks/Depth types schematics.py
+++ notebooks/Depth types schematics.py
@@ -6,54 +6,95 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.3.0
+#       jupytext_version: 1.3.2
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python [conda env:eql_source_layouts]
 #     language: python
-#     name: python3
+#     name: conda-env-eql_source_layouts-py
 # ---
 
 # +
+from IPython.display import display
 import os
-import pyproj
 import numpy as np
 import pandas as pd
-import verde as vd
-import harmonica as hm
 import matplotlib.pyplot as plt
 
-from eql_source_layouts.layouts import (
-    source_bellow_data
-)
+from eql_source_layouts.layouts import source_bellow_data
+
 # -
 
 survey = pd.read_csv(os.path.join("..", "data", "survey_1d.csv"))
+display(survey)
 
-survey
+points = {}
 
 # +
+depth_type = "constant_depth"
 coordinates = (survey.easting, np.zeros_like(survey.easting), survey.height)
-points = source_bellow_data(coordinates, depth_type="constant_depth", constant_depth=100)
+points[depth_type] = source_bellow_data(
+    coordinates, depth_type=depth_type, constant_depth=150
+)
 
 plt.scatter(coordinates[0], coordinates[2])
-plt.scatter(points[0], points[2])
+plt.scatter(points[depth_type][0], points[depth_type][2])
 plt.show()
 
 # +
+depth_type = "relative_depth"
 coordinates = (survey.easting, np.zeros_like(survey.easting), survey.height)
-points = source_bellow_data(coordinates, depth_type="relative_depth", relative_depth=100)
+points[depth_type] = source_bellow_data(
+    coordinates, depth_type=depth_type, relative_depth=150
+)
 
 plt.scatter(coordinates[0], coordinates[2])
-plt.scatter(points[0], points[2])
+plt.scatter(points[depth_type][0], points[depth_type][2])
 plt.show()
 
 # +
+depth_type = "variable_relative_depth"
 coordinates = (survey.easting, np.zeros_like(survey.easting), survey.height)
-points = source_bellow_data(coordinates, depth_type="variable_relative_depth", depth_factor=1, depth_shift=-100, k_nearest=3)
+points[depth_type] = source_bellow_data(
+    coordinates, depth_type=depth_type, depth_factor=1, depth_shift=-100, k_nearest=3
+)
 
 plt.scatter(coordinates[0], coordinates[2])
-plt.scatter(points[0], points[2])
+plt.scatter(points[depth_type][0], points[depth_type][2])
+plt.show()
+# +
+plt.style.use(os.path.join("..", "matplotlib.rc"))
+fig, axes = plt.subplots(ncols=3, nrows=1, sharey=True, figsize=(6.66, 2), dpi=300)
+size = 8
+
+titles = ["Constant Depth", "Relative Depth", "Variable Relative Depth"]
+labels = "(a) (b) (c)".split()
+
+for i, (ax, depth_type, title, label) in enumerate(zip(axes, points, titles, labels)):
+    ax.scatter(coordinates[0], coordinates[2], s=size, label="stations", marker="^")
+    ax.scatter(points[depth_type][0], points[depth_type][2], s=size, label="sources")
+    ax.axhline(y=0, color="grey", linestyle="--", linewidth=0.5)
+    ax.set_title(title)
+    ax.set_aspect("equal")
+    ax.tick_params(
+        axis="y",  # changes apply to the y-axis
+        which="both",  # both major and minor ticks are affected
+        left=False,  # ticks along the left edge are off
+        right=False,  # ticks along the right edge are off
+        labelleft=False,  # labels along the bottom edge are off
+    )
+    ax.tick_params(
+        axis="x",  # changes apply to the y-axis
+        which="both",  # both major and minor ticks are affected
+        bottom=False,  # ticks along the left edge are off
+        top=False,  # ticks along the right edge are off
+        labelbottom=False,  # labels along the bottom edge are off
+    )
+    ax.annotate(label, xy=(0.03, 0.88), xycoords="axes fraction")
+
+axes[0].legend(loc=(0.6, 0.32), borderpad=0.2, labelspacing=0.3)
+axes[0].set_ylim(-200, 130)
+
+plt.tight_layout(w_pad=0)
+plt.savefig(os.path.join("..", "manuscript", "figs", "depth_types.pdf"))
 plt.show()
 # -
-
-
