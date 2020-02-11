1891a2bff7333dd9df4bb1c9b10d4d47e6281958
Santiago Soler <santiago.r.soler@gmail.com>
2020-02-11 10:12:11 -0300

    Improve block-median-schematics.py notebook

--- notebooks/block-median-schematics.py
+++ notebooks/block-median-schematics.py
@@ -6,12 +6,16 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.3.3
+#       jupytext_version: 1.3.2
 #   kernelspec:
 #     display_name: Python [conda env:eql_source_layouts]
 #     language: python
 #     name: conda-env-eql_source_layouts-py
 # ---
+
+# # Schematic figure of block-median sources
+
+# Import packages
 
 # +
 import os
@@ -20,6 +24,10 @@ import verde as vd
 import matplotlib.pyplot as plt
 
 from eql_source_layouts import block_median_sources
+
+# -
+
+# Define parameters
 
 # +
 region = (-50e3, 50e3, -50e3, 50e3)
@@ -31,13 +39,19 @@ height = 1000
 depth = 100
 # -
 
+# Create random observation points
+
 coordinates = vd.scatter_points(
     region, size=n_data, random_state=seed, extra_coords=height
 )
 
+# Create block-median sources
+
 points = block_median_sources(
     coordinates, constant_depth=depth, spacing=spacing, depth_type="constant_depth"
 )
+
+# Create grid lines
 
 # +
 grid_nodes = vd.grid_coordinates(vd.get_region(coordinates), spacing=spacing)
@@ -45,23 +59,32 @@ grid_nodes = vd.grid_coordinates(vd.get_region(coordinates), spacing=spacing)
 grid_lines = (np.unique(grid_nodes[0]), np.unique(grid_nodes[1]))
 for nodes in grid_lines:
     nodes.sort()
+# -
+
+# Plot figure
 
 # +
+# Load matplotlib configuration
+plt.style.use(os.path.join("..", "matplotlib.rc"))
+
+# Boundaries of the grid
 xmin, xmax, ymin, ymax = vd.get_region(coordinates)
-grid_style = dict(color="grey", linewidth=0.7, linestyle="--")
+
+# Define styles and axes labels
+grid_style = dict(color="grey", linewidth=1, linestyle="--")
 scatter_style = dict(s=12)
 labels = "a b c".split()
 
-plt.style.use(os.path.join("..", "matplotlib.rc"))
+# Initialize figure and axes
+width = 6.66
+height = width / 3 * 1.1
+fig, axes = plt.subplots(ncols=3, nrows=1, sharey=True, figsize=(width, height),)
 
-fig, axes = plt.subplots(
-    ncols=3, nrows=1, sharey=True, figsize=(6.66, 6.66 / 3), dpi=300
-)
-
+# Plot coordinates
 ax = axes[0]
 ax.scatter(*coordinates[:2], label="data points", **scatter_style)
 
-
+# Plot coordinates and grid
 ax = axes[1]
 for x in grid_lines[0]:
     ax.plot((x, x), (ymin, ymax), **grid_style)
@@ -69,6 +92,7 @@ for y in grid_lines[1]:
     ax.plot((xmin, xmax), (y, y), **grid_style)
 ax.scatter(*coordinates[:2], **scatter_style)
 
+# Plot block-median sources and grid
 ax = axes[2]
 for x in grid_lines[0]:
     ax.plot((x, x), (ymin, ymax), **grid_style)
@@ -76,7 +100,7 @@ for y in grid_lines[1]:
     ax.plot((xmin, xmax), (y, y), **grid_style)
 ax.scatter(*points[:2], color="C1", label="block-median points", **scatter_style)
 
-
+# Configure axes
 for ax, label in zip(axes, labels):
     ax.annotate(
         label,
@@ -86,22 +110,15 @@ for ax, label in zip(axes, labels):
     )
     ax.set_aspect("equal")
     ax.tick_params(
-        axis="y",  # changes apply to the y-axis
-        which="both",  # both major and minor ticks are affected
-        left=False,  # ticks along the left edge are off
-        right=False,  # ticks along the right edge are off
-        labelleft=False,  # labels along the bottom edge are off
+        axis="y", which="both", left=False, right=False, labelleft=False,
     )
     ax.tick_params(
-        axis="x",  # changes apply to the y-axis
-        which="both",  # both major and minor ticks are affected
-        bottom=False,  # ticks along the left edge are off
-        top=False,  # ticks along the right edge are off
-        labelbottom=False,  # labels along the bottom edge are off
+        axis="x", which="both", bottom=False, top=False, labelbottom=False,
     )
 
-# axes[0].legend(loc=3)
-# axes[2].legend(loc=3)
 plt.tight_layout(w_pad=0)
+plt.savefig(
+    os.path.join("..", "manuscript", "figs", "block-median-sources-schematics.pdf")
+)
 plt.show()
 # -
