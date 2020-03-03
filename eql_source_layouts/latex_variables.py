d8090ca63594ee814ea0f09d102457b2fa61fcf2
Santiago Soler <santiago.r.soler@gmail.com>
2020-03-03 15:19:50 -0300

    Rewrite functions to convert parameters to LaTeX variables
    Make use of LaTeX numlist and numrange to create ranges and intervals as
    LaTeX variables.

--- eql_source_layouts/latex_variables.py
+++ eql_source_layouts/latex_variables.py
@@ -23,20 +23,23 @@ def latex_variables(variable, number, unit=None, fmt=None):
 
 def latex_parameters(parameters, survey):
     """
-    Create list of parameters for each source distribution as LaTeX variables
+    Create list or ranges of parameters for each source distribution as LaTeX variables
     """
     tex_variables = []
     for layout in parameters:
         for depth_type in parameters[layout]:
             for parameter in parameters[layout][depth_type]:
                 if parameter != "depth_type":
-                    values = format_parameters(
+                    values, interval = format_parameters(
                         parameters[layout][depth_type][parameter], parameter
                     )
                     variable_name = format_variable_name(
                         "_".join([survey, layout, depth_type, parameter])
                     )
                     tex_variables.append(latex_variables(variable_name, values))
+                    tex_variables.append(
+                        latex_variables(variable_name + "Interval", interval)
+                    )
     return tex_variables
 
 
@@ -49,26 +52,77 @@ def format_variable_name(name):
     return name.replace("_", " ").title().replace(" ", "")
 
 
-def format_parameters(parameters_list, parameter_name):
+def format_parameters(parameters, parameter_name):
     """
-    Format a list of parameters to appear on the table
+    Format a list or range of parameters to appear on the table
     """
-    fmt = ""
-    # Convert spacings and depths from m to km and configure string format
-    if parameter_name in ("spacing", "depth"):
-        parameters_list = np.array(parameters_list) * 1e-3
-        if min(parameters_list) < 1:  # asign formats if min value is lower than 1 km
-            fmt = ".2f"
+    parameters = np.array(parameters, dtype=float)
+
+    # Format damping
+    if parameter_name == "damping":
+        values, interval = _format_damping(parameters)
+        return values, interval
+
+    # Convert depths and spacings to km
+    if parameter_name in ("depth", "spacing"):
+        parameters *= 1e-3
+
+    # Check if parameters are given in ranges
+    differences = parameters[1:] - parameters[:-1]
+    if np.allclose(differences, differences[0]):
+        values, interval = _create_numrange(parameters)
+    else:
+        return _create_numlist(parameters)
+    return values, interval
+
+
+def _create_numrange(parameters):
+    """
+    Create range of values using LaTeX numrange
+    """
+    interval = parameters[1] - parameters[0]
+    # Set format
+    if parameters.min() < 1:
+        fmt = ".1f"
+    else:
+        fmt = ".0f"
+    values = r"\numrange{{{min:>{fmt}}}}{{{max:>{fmt}}}}".format(
+        min=parameters.min(), max=parameters.max(), fmt=fmt
+    )
+    interval = r"\num{{{interval:>{fmt}}}}".format(interval=interval, fmt=fmt)
+    return values, interval
+
+
+def _create_numlist(parameters, fmt=None):
+    """
+    Create list of values using LaTeX numlist
+    """
+    values = []
+    for value in parameters:
+        if value == 0:
+            fmt = ".0f"
+        if value < 1:
+            fmt = ".1f"
         else:
             fmt = ".0f"
-    # Create a string of parameters separated by commas
-    values = []
-    for p in parameters_list:
-        if p is None:
-            values.append("{}".format(p))
-        elif p == 0:
-            values.append("0")
-        else:
-            values.append("{p:>{fmt}}".format(p=p, fmt=fmt))
-    values = ", ".join(values)
-    return values
+        values.append("{v:>{fmt}}".format(v=value, fmt=fmt))
+    values = ";".join(values)
+    values = r"\numlist{{{values}}}".format(values=values)
+    interval = "-"
+    return values, interval
+
+
+def _format_damping(parameters):
+    """
+    Create range of values for damping parameters
+    """
+    fmt = ".0f"
+    parameters = np.log10(parameters)
+    differences = parameters[1:] - parameters[:-1]
+    interval = differences[1] - differences[0]
+    assert np.allclose(differences[0], differences)
+    values = r"\numrange{{10$^{min:>{fmt}}$}}{{10$^{max:>{fmt}}$}}".format(
+        min=parameters.min(), max=parameters.max(), fmt=fmt
+    )
+    interval = r"\num{{10$^{interval:>{fmt}}$}}".format(interval=interval, fmt=fmt)
+    return values, interval
