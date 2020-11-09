826633c3986030440dbb448541f9776a80332f4b
Santiago Soler <santiago.r.soler@gmail.com>
2020-11-09 14:19:31 -0300

    Update South Africa notebook

--- notebooks/09-grid-south-africa.py
+++ notebooks/09-grid-south-africa.py
@@ -191,11 +191,10 @@ points = block_averaged_sources(coordinates, **best_parameters)
 eql = hm.EQLHarmonic(damping=best_parameters["damping"], points=points)
 eql.fit(coordinates, gravity_disturbance)
 
-eql.extra_coords_name = "upward"
 grid = eql.grid(
+    upward=3000,
     region=vd.get_region((data.longitude, data.latitude)),
     spacing=0.05,
-    extra_coords=3000,
     data_names=["gravity_disturbance"],
     projection=projection,
 )
@@ -355,11 +354,10 @@ eql = EQLIterative(
 for _ in range(3):
     eql.fit(coordinates, gravity_disturbance)
 
-eql.extra_coords_name = "upward"
 grid_iterative = eql.grid(
+    upward=3000,
     region=vd.get_region((data.longitude, data.latitude)),
     spacing=0.05,
-    extra_coords=3000,
     data_names=["gravity_disturbance"],
     projection=projection,
 )
