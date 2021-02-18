73de122424d00b667ae81d37b2437f7e7fe272ad
Santiago Soler <santiago.r.soler@gmail.com>
2021-02-18 10:03:56 -0300

    Add schematic figure of gradient boosting (#123)
    Add a sketch that symbolizes the gradient-boosting process using the synthetic
    ground survey. Add reference to the figure on the text. Renames the notebook
    that creates the schematic figures. Only the figure showing the smaller sets of
    sources is created through the notebook. the final figure was created through
    Inkscape by adding the observation points and the location of the original
    sources.

--- notebooks/02-schematics-figures.py
+++ notebooks/02-schematics-figures.py
@@ -6,12 +6,14 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.7.1
+#       jupytext_version: 1.9.1
 #   kernelspec:
 #     display_name: Python [conda env:eql-gradient-boosted]
 #     language: python
 #     name: conda-env-eql-gradient-boosted-py
 # ---
+
+# # Source layouts schematics
 
 # +
 from IPython.display import display  # noqa: F401  # ignore used but not imported
@@ -20,6 +22,7 @@ import numpy as np
 import pandas as pd
 import verde as vd
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 import boost_and_layouts
 from boost_and_layouts import save_to_json
@@ -130,7 +133,7 @@ for ax, label in zip(axes, labels):
 
 # Plot observation points
 ax = axes[0]
-ax.scatter(survey.easting, survey.northing, s=size, c="C0")
+ax.scatter(survey.easting, survey.northing, s=size, c="C0", marker="^")
 ax.set_title("Observation points")
 
 # Plot location of sources for each source layout
@@ -169,3 +172,146 @@ for layout in layouts:
 
 json_file = results_dir / "source-layouts-schematics.json"
 save_to_json(variables, json_file)
+# -
+
+# # Gradient boosting schematics
+
+sources = source_distributions["source_below_data"]
+region = vd.get_region(sources)
+
+# +
+overlapping = 0.5
+window_size = 18e3
+spacing = window_size * (1 - overlapping)
+
+centers, indices = vd.rolling_window(sources, size=window_size, spacing=spacing)
+spacing_easting = centers[0][0, 1] - centers[0][0, 0]
+spacing_northing = centers[1][1, 0] - centers[1][0, 0]
+
+print("Desired spacing:", spacing)
+print("Actual spacing:", (spacing_easting, spacing_northing))
+
+# +
+indices = [i[0] for i in indices.ravel()]
+centers = [i.ravel() for i in centers]
+n_windows = centers[0].size
+
+print("Number of windows:", n_windows)
+
+# +
+ncols = 10
+figsize = (1.7 * ncols, 1.7)
+size = 3
+
+fig, axes = plt.subplots(
+    ncols=ncols, nrows=1, figsize=figsize, dpi=300, sharex=True, sharey=True
+)
+
+for ax in axes:
+    ax.set_aspect("equal")
+    ax.axis("off")
+
+# Observation points
+axes[0].scatter(survey.easting, survey.northing, s=size, c="C0", marker="^")
+
+# Sources
+axes[1].scatter(*sources[:2], s=size, c="C1")
+
+# First fit
+# ---------
+window_i = 0
+window = indices[window_i]
+not_window = [i for i in np.arange(sources[0].size) if i not in window]
+w_center_easting, w_center_northing = centers[0][window_i], centers[1][window_i]
+rectangle_kwargs = dict(
+    xy=(w_center_easting - window_size / 2, w_center_northing - window_size / 2),
+    width=window_size,
+    height=window_size,
+    fill=False,
+    linewidth=0.5,
+    linestyle="--",
+    color="#444444",
+)
+
+# Observation points
+axes[2].scatter(
+    survey.easting.values[window],
+    survey.northing.values[window],
+    s=size,
+    c="C0",
+    marker="^",
+)
+axes[2].scatter(
+    survey.easting.values[not_window],
+    survey.northing.values[not_window],
+    s=size,
+    c="C7",
+    marker="^",
+)
+rectangle = Rectangle(**rectangle_kwargs)
+axes[2].add_patch(rectangle)
+
+# Sources
+axes[3].scatter(sources[0][window], sources[1][window], s=size, c="C1")
+axes[3].scatter(sources[0][not_window], sources[1][not_window], s=size, c="C7")
+rectangle = Rectangle(**rectangle_kwargs)
+axes[3].add_patch(rectangle)
+
+# First Prediction
+# ----------------
+axes[4].scatter(survey.easting, survey.northing, s=size, c="C3", marker="v")
+axes[5].scatter(sources[0][window], sources[1][window], s=size, c="C1")
+rectangle = Rectangle(**rectangle_kwargs)
+axes[5].add_patch(rectangle)
+
+# Second fit
+# ----------
+window_i = 3
+window = indices[window_i]
+not_window = [i for i in np.arange(sources[0].size) if i not in window]
+w_center_easting, w_center_northing = centers[0][window_i], centers[1][window_i]
+rectangle_kwargs = dict(
+    xy=(w_center_easting - window_size / 2, w_center_northing - window_size / 2),
+    width=window_size,
+    height=window_size,
+    fill=False,
+    linewidth=0.5,
+    linestyle="--",
+    color="#444444",
+)
+
+# Residue
+axes[6].scatter(
+    survey.easting.values[window],
+    survey.northing.values[window],
+    s=size,
+    c="C3",
+    marker="v",
+)
+axes[6].scatter(
+    survey.easting.values[not_window],
+    survey.northing.values[not_window],
+    s=size,
+    c="C7",
+    marker="^",
+)
+rectangle = Rectangle(**rectangle_kwargs)
+axes[6].add_patch(rectangle)
+
+# Sources
+axes[7].scatter(sources[0][window], sources[1][window], s=size, c="C1")
+axes[7].scatter(sources[0][not_window], sources[1][not_window], s=size, c="C7")
+rectangle = Rectangle(**rectangle_kwargs)
+axes[7].add_patch(rectangle)
+
+# Second Prediction
+# -----------------
+axes[8].scatter(survey.easting, survey.northing, s=size, c="C3", marker="v")
+axes[9].scatter(sources[0][window], sources[1][window], s=size, c="C1")
+rectangle = Rectangle(**rectangle_kwargs)
+axes[9].add_patch(rectangle)
+
+
+plt.savefig(Path("..") / "manuscript" / "figs" / "svg" / "gradient-boosting-raw.svg")
+plt.show()
+# -
